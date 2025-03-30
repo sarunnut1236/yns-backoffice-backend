602d8d75be0f246c5d341b21f63bc75c7d13c45d
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2025-03-30 22:14:08 +0700

    connect to db

--- src/adapters/database/data-source.ts
+++ src/adapters/database/data-source.ts
@@ -4,16 +4,12 @@ import { Cars, User } from "../../core/entity";
 
 configDotenv();
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } =
+const { DATABASE_URL, NODE_ENV } =
     process.env;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: DB_HOST,
-    port: parseInt(DB_PORT || "5432"),
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
+    url: DATABASE_URL,
     synchronize: NODE_ENV === "development" ? true : false,
     logging: NODE_ENV === "development" ? true : false,
     entities: [User, Cars],
